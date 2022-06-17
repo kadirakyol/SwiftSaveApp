ce796db41930c8894c5630f096976de90e2c28af
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-17 09:52:02 +0300

    first commit

--- BirthdayyNoteTaker/ViewController.swift
+++ BirthdayyNoteTaker/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var birthDayTextField: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        if let  name = nameTextField.text {
+            if let birthDay = birthDayTextField.text {
+                
+                UserDefaults.standard.set(name, forKey: "name")
+                UserDefaults.standard.set(birthDay, forKey: "birthDay")
+                
+                nameLabel.text = "Name : \(name)"
+                birthDayLabel.text = "BirthDay : \(birthDay)"
+                
+            }
+        }
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthDayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthDay = UserDefaults.standard.object(forKey: "birthDay")
+        
+        if let newName = storedName as? String {
+            nameLabel.text = "Name: \(newName)"
+        }
+        if let newBirthDay = storedBirthDay as? String {
+            birthDayLabel.text = "BirthDay: \(newBirthDay)"
+        }
+        
     }
-
-
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthDay = UserDefaults.standard.object(forKey: "birthDay")
+        
+        if let newName = storedName as? String {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name: "
+        }
+        
+        if (storedBirthDay as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthDay")
+            birthDayLabel.text = "BirthDay: "
+        }
+        
+    }
+    
 }
 
